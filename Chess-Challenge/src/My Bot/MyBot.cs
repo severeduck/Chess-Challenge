a45bb8c829a756ed10afeff4bdf136b9a58bc239
severeduck <92829782+severeduck@users.noreply.github.com>
2023-08-26 22:33:08 +0200

    upd

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,50 +5,41 @@ using System.Linq;
 
 public class MyBot : IChessBot
 {
-    private const int QUIESCENCE_MAX_DEPTH = 2;
+    private const int QUIESCENCE_MAX_DEPTH = 3;
     private Timer _timer;
-
-    public MyBot()
-    {
-    }
 
     public Move Think(Board board, Timer timer)
     {
         _timer = timer;
-
         var legalMoves = board.GetLegalMoves().ToList();
+
         if (!legalMoves.Any())
-        {
-            return default; // No legal moves, which means it's either a checkmate or stalemate.
-        }
+            return default;
 
-        if (board.PlyCount <= 2)
-        {
-            legalMoves = legalMoves.OrderBy(x => Guid.NewGuid()).ToList();  // Shuffle moves
-        }
-
-        int depth = board.PlyCount <= 2 ? 3 : 1;  // If it's the first or second move, start at depth 3. 
+        int depth = CalculateDynamicDepth(timer);
         Move bestMove = legalMoves.First();
 
-        double timeForThisMove = _timer.MillisecondsRemaining / (double)40; // Assume we'll make 40 more moves
+        double timeForThisMove = _timer.MillisecondsRemaining / 50.0; // Adjusted from 40 for better endgame performance
         DateTime startTime = DateTime.Now;
 
-        // Iterative deepening
-        while ((DateTime.Now - startTime).TotalMilliseconds < timeForThisMove && depth <= 5) // Limiting maximum depth to 5 for safety
+        while ((DateTime.Now - startTime).TotalMilliseconds < timeForThisMove && depth <= 6)  // Depth limit increased to 6
         {
             bestMove = AlphaBetaSearch(board, legalMoves, depth++);
         }
 
         return bestMove;
     }
+
     private Move AlphaBetaSearch(Board board, List<Move> legalMoves, int depth)
     {
         Move bestMove = legalMoves.First();
         double alpha = double.NegativeInfinity;
         double beta = double.PositiveInfinity;
 
-        // Simple move ordering: prioritize captures.
-        legalMoves = legalMoves.OrderByDescending(move => move.IsCapture).ToList();
+        // Enhanced move ordering: prioritize captures and then move those with the highest history scores (implementing a simple history heuristic)
+        legalMoves = legalMoves.OrderByDescending(move => move.IsCapture)
+                               .ThenByDescending(move => MoveHistoryScore(move))
+                               .ToList();
 
         foreach (var move in legalMoves)
         {
@@ -66,9 +57,19 @@ public class MyBot : IChessBot
         return bestMove;
     }
 
+    // Implement a simple history heuristic. 
+    // This is a very rudimentary version, where we remember how often a move has contributed to beta cutoffs.
+    private static Dictionary<Move, int> moveHistory = new Dictionary<Move, int>();
+    private int MoveHistoryScore(Move move)
+    {
+        if (moveHistory.ContainsKey(move))
+            return moveHistory[move];
+        return 0;
+    }
+
     private double AlphaBeta(Board board, int depth, double alpha, double beta)
     {
-        if (depth == 0 || _timer.MillisecondsRemaining <= (_timer.GameStartTimeMilliseconds / (double)board.PlyCount))
+        if (depth == 0 || _timer.MillisecondsRemaining <= (_timer.GameStartTimeMilliseconds / 1024.0))
             return QuiescenceSearch(board, alpha, beta, QUIESCENCE_MAX_DEPTH);
 
         foreach (var move in board.GetLegalMoves())
@@ -78,38 +79,17 @@ public class MyBot : IChessBot
             board.UndoMove(move);
 
             if (score >= beta)
+            {
+                // Record the move that caused the cutoff
+                if (!moveHistory.ContainsKey(move))
+                    moveHistory[move] = 0;
+                moveHistory[move]++;
+
                 return beta;
+            }
 
             if (score > alpha)
-            {
                 alpha = score;
-                if (alpha >= beta)
-                    break;  // Prune the search tree.
-            }
-        }
-
-        return alpha;
-    }
-
-    private double QuiescenceSearch(Board board, double alpha, double beta, int depth)
-    {
-        if (depth == 0 || _timer.MillisecondsRemaining <= (_timer.GameStartTimeMilliseconds / (double)board.PlyCount))
-            return EvaluateBoard(board);
-
-        foreach (var move in board.GetLegalMoves())
-        {
-            if (move.IsCapture)
-            {
-                board.MakeMove(move);
-                double score = -QuiescenceSearch(board, -beta, -alpha, depth - 1);
-                board.UndoMove(move);
-
-                if (score >= beta)
-                    return beta;
-
-                if (score > alpha)
-                    alpha = score;
-            }
         }
 
         return alpha;
@@ -119,51 +99,54 @@ public class MyBot : IChessBot
     {
         double score = 0;
 
-        double pawnValue = 1.0;
-        double knightValue = 3.0;
-        double bishopValue = 3.0;
-        double rookValue = 5.0;
-        double queenValue = 9.0;
-        double kingSafetyValue = -20.0;
+        // ... [your existing evaluation code here]
 
+        // Doubled Pawns
         var whitePawns = board.GetPieceList(PieceType.Pawn, true).ToList();
         var blackPawns = board.GetPieceList(PieceType.Pawn, false).ToList();
+        score -= whitePawns.GroupBy(p => p.Square.File).Where(g => g.Count() > 1).Count() * 0.5;
+        score += blackPawns.GroupBy(p => p.Square.File).Where(g => g.Count() > 1).Count() * 0.5;
 
-        // Basic material count
-        score += (whitePawns.Count - blackPawns.Count) * pawnValue;
-        score += (board.GetPieceList(PieceType.Knight, true).Count - board.GetPieceList(PieceType.Knight, false).Count) * knightValue;
-        score += (board.GetPieceList(PieceType.Bishop, true).Count - board.GetPieceList(PieceType.Bishop, false).Count) * bishopValue;
-        score += (board.GetPieceList(PieceType.Rook, true).Count - board.GetPieceList(PieceType.Rook, false).Count) * rookValue;
-        score += (board.GetPieceList(PieceType.Queen, true).Count - board.GetPieceList(PieceType.Queen, false).Count) * queenValue;
+        // Mobility
+        score += 0.1 * board.GetLegalMoves(true).Count();
+        score -= 0.1 * board.GetLegalMoves(false).Count();
 
-        // King Safety
-        if (board.IsInCheck())
+        // Endgame considerations
+        if (GetPieceCount(board, true) <= 7 && GetPieceCount(board, false) <= 7)
         {
-            score += board.IsWhiteToMove ? kingSafetyValue : -kingSafetyValue;
+            // In the endgame, the king should be more active
+            var whiteKing = board.GetPieceList(PieceType.King, true).First();
+            var blackKing = board.GetPieceList(PieceType.King, false).First();
+            score += 0.3 * (7 - DistanceToCenter(whiteKing.Square));
+            score -= 0.3 * (7 - DistanceToCenter(blackKing.Square));
         }
-
-        // Pawn Structures
-        foreach (var pawn in whitePawns)
-        {
-            if (IsIsolatedPawn(pawn, whitePawns))
-                score -= 0.5;
-            if (IsPassedPawn(pawn, blackPawns))
-                score += 1;
-        }
-        foreach (var pawn in blackPawns)
-        {
-            if (IsIsolatedPawn(pawn, blackPawns))
-                score += 0.5;
-            if (IsPassedPawn(pawn, whitePawns))
-                score -= 1;
-        }
-
-        // Central Control
-        double centralControlValue = 0.5;
-        score += whitePawns.Count(p => p.Square.File == 'd' || p.Square.File == 'e') * centralControlValue;
-        score -= blackPawns.Count(p => p.Square.File == 'd' || p.Square.File == 'e') * centralControlValue;
 
         return board.IsWhiteToMove ? score : -score;
+    }
+
+    private int GetPieceCount(Board board, bool isWhite)
+    {
+        int startIndex = isWhite ? 0 : 6;  // 0 for white, 6 for black
+        int endIndex = isWhite ? 5 : 11;   // 5 for white, 11 for black
+
+        int count = 0;
+
+        var allPieceLists = board.GetAllPieceLists();
+        for (int i = startIndex; i <= endIndex; i++)
+        {
+            count += allPieceLists[i].Count();
+        }
+
+        return count;
+    }
+
+    private double DistanceToCenter(Square square)
+    {
+        int dx = Math.Min(Math.Abs(square.File - 'd'), Math.Abs(square.File - 'e'));
+        int dy = Math.Min(Math.Abs(square.Rank - 4), Math.Abs(square.Rank - 5));
+
+        // Using Euclidean distance for simplicity
+        return Math.Sqrt(dx * dx + dy * dy);
     }
 
     private bool IsIsolatedPawn(Piece pawn, List<Piece> pawnsOfSameColor)
@@ -189,5 +172,40 @@ public class MyBot : IChessBot
             return 2;
         else
             return 3;
+    }
+
+    private double QuiescenceSearch(Board board, double alpha, double beta, int depthLeft)
+    {
+        if (depthLeft == 0 || _timer.MillisecondsRemaining <= (_timer.GameStartTimeMilliseconds / 1024.0))
+        {
+            return EvaluateBoard(board);
+        }
+
+        double standPat = EvaluateBoard(board);
+        if (standPat >= beta)
+        {
+            return beta;
+        }
+        if (alpha < standPat)
+        {
+            alpha = standPat;
+        }
+
+        foreach (var move in board.GetLegalMoves().Where(m => m.IsCapture))
+        {
+            board.MakeMove(move);
+            double score = -QuiescenceSearch(board, -beta, -alpha, depthLeft - 1);
+            board.UndoMove(move);
+
+            if (score >= beta)
+            {
+                return beta;
+            }
+            if (score > alpha)
+            {
+                alpha = score;
+            }
+        }
+        return alpha;
     }
 }
