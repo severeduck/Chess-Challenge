a448b907478c6d874915c2ea16d6e8901f668f7c
severeduck <92829782+severeduck@users.noreply.github.com>
2023-08-26 13:17:42 +0200

    a simple solution

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,88 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        int maxDepth = 4;  // or calculate dynamically based on timer
+        Move bestMove = Move.NullMove;
+
+        int alpha = int.MinValue;
+        int beta = int.MaxValue;
+
+        var legalMoves = board.GetLegalMoves(false);
+
+        // If there are no legal moves, just return the null move.
+        if (legalMoves.Length == 0)
+        {
+            return Move.NullMove;
+        }
+
+        foreach (var move in legalMoves)
+        {
+            board.MakeMove(move);
+            int moveValue = -AlphaBeta(board, maxDepth - 1, -beta, -alpha);
+            board.UndoMove(move);
+            if (moveValue > alpha)
+            {
+                alpha = moveValue;
+                bestMove = move;
+            }
+        }
+
+        // Check if the best move is in the legal moves list.
+        if (!Array.Exists(legalMoves, m => m.Equals(bestMove)))
+        {
+            throw new Exception("Invalid best move detected!");
+        }
+
+        return bestMove;
+    }
+    int AlphaBeta(Board board, int depth, int alpha, int beta)
+    {
+        if (depth == 0 || board.IsDraw() || board.IsInCheckmate())
+            return EvaluateBoard(board);
+
+        var legalMoves = board.GetLegalMoves(false);
+        foreach (var move in legalMoves)
+        {
+            board.MakeMove(move);
+            int score = -AlphaBeta(board, depth - 1, -beta, -alpha);
+            board.UndoMove(move);
+
+            if (score >= beta)
+                return beta;
+            if (score > alpha)
+                alpha = score;
+        }
+        return alpha;
+    }
+
+    int EvaluateBoard(Board board)
+    {
+        // A simple evaluation function for demonstration.
+        // Here, you would use your own logic to assign a score to the board.
+        // For now, we'll simply count the material difference.
+        int value = 0;
+
+        // Assign piece values and sum up
+        foreach (var pieceList in board.GetAllPieceLists())
+        {
+            int pieceValue;
+            switch (pieceList.TypeOfPieceInList)
+            {
+                case PieceType.Pawn: pieceValue = 1; break;
+                case PieceType.Knight: pieceValue = 3; break;
+                case PieceType.Bishop: pieceValue = 3; break;
+                case PieceType.Rook: pieceValue = 5; break;
+                case PieceType.Queen: pieceValue = 9; break;
+                default: pieceValue = 0; break;
+            }
+
+            value += pieceList.IsWhitePieceList ? pieceValue * pieceList.Count : -pieceValue * pieceList.Count;
+        }
+
+        return board.IsWhiteToMove ? value : -value;
     }
 }
