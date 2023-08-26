6ebec184534bf89f7f67ca0502abd0b7843393fd
severeduck <92829782+severeduck@users.noreply.github.com>
2023-08-26 14:07:07 +0200

    upd

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,90 +1,84 @@
-﻿using ChessChallenge.API;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
+    private const int CheckmateScore = 100000;
+    private readonly OpeningBook _openingBook;
+    private Dictionary<string, int> _evaluatedPositions;
+
+    public MyBot()
+    {
+        _openingBook = new OpeningBook();
+        _evaluatedPositions = new Dictionary<string, int>();
+    }
 
     public Move Think(Board board, Timer timer)
     {
-        DateTime startTime = DateTime.Now;
-        TimeSpan timeForMove = TimeSpan.FromSeconds(2); // Temporary heuristic. Adjust as needed.
-        Move bestMove = Move.NullMove;
-
-        var legalMoves = board.GetLegalMoves(false);
-
-        // If no legal moves, return NullMove
-        if (legalMoves.Length == 0) return Move.NullMove;
-
-        for (int depth = 1; depth <= 100; depth++) // 100 or whatever max depth you choose
-        {
-            Move currentBestMove = DepthLimitedSearch(board, depth);
-            if (DateTime.Now - startTime > timeForMove)
-                break;
-            if (currentBestMove != Move.NullMove)
-                bestMove = currentBestMove;
-        }
-
-        // If we don't have a best move from the search, pick the first legal move.
-        if (bestMove == Move.NullMove)
-            bestMove = legalMoves[0];
-
+        _evaluatedPositions.Clear();
+        var bestMove = MinimaxRoot(board, CalculateDynamicDepth(board));
         return bestMove;
     }
 
-    public Move DepthLimitedSearch(Board board, int depth)
+    private int CalculateDynamicDepth(Board board)
     {
-        int maxDepth = depth;
-        Move bestMove = Move.NullMove;
+        // A very simple logic to increase depth as the game progresses. 
+        // This can be expanded or replaced with a more robust approach.
+        int totalPieces = board.WhitePieces.Count() + board.BlackPieces.Count();
+        return totalPieces > 25 ? 3 : 4; // Adjust values as per testing
+    }
 
-        int alpha = int.MinValue;
-        int beta = int.MaxValue;
+    private Move MinimaxRoot(Board board, int depth)
+    {
+        var validMoves = board.GetValidMovesForCurrentPlayer();
+        int bestMoveScore = int.MinValue;
+        Move bestMove = validMoves.First();
 
-        var legalMoves = board.GetLegalMoves(false);
-        foreach (var move in legalMoves)
+        foreach (var move in validMoves)
         {
             board.MakeMove(move);
-            int moveValue = -AlphaBeta(board, maxDepth - 1, -beta, -alpha);
-            board.UndoMove(move);
-            if (moveValue > alpha)
+            var boardValue = -AlphaBeta(board, depth - 1, int.MinValue, int.MaxValue);
+            board.UndoLastMove();
+
+            if (boardValue > bestMoveScore)
             {
-                alpha = moveValue;
+                bestMoveScore = boardValue;
                 bestMove = move;
             }
         }
-
-        // If no move was better than the initial alpha, return a NullMove
-        if (alpha == int.MinValue) return Move.NullMove;
-
         return bestMove;
     }
 
-
-
-    int AlphaBeta(Board board, int depth, int alpha, int beta)
+    private int AlphaBeta(Board board, int depth, int alpha, int beta)
     {
         if (depth == 0)
-            return QuiescenceSearch(board, alpha, beta);
+            return QuiescenceSearch(board, alpha, beta, depth);
 
-        if (board.IsDraw() || board.IsInCheckmate())
-            return EvaluateBoard(board);
+        var validMoves = board.GetValidMovesForCurrentPlayer();
 
-        var legalMoves = board.GetLegalMoves(false);
-        foreach (var move in legalMoves)
+        if (!validMoves.Any())
+        {
+            return board.CurrentPlayer == board.WhitePlayer ? -CheckmateScore : CheckmateScore;
+        }
+
+        foreach (var move in validMoves)
         {
             board.MakeMove(move);
-            int score = -AlphaBeta(board, depth - 1, -beta, -alpha);
-            board.UndoMove(move);
+            int boardValue = -AlphaBeta(board, depth - 1, -beta, -alpha);
+            board.UndoLastMove();
 
-            if (score >= beta)
+            if (boardValue >= beta)
                 return beta;
-            if (score > alpha)
-                alpha = score;
+            if (boardValue > alpha)
+                alpha = boardValue;
         }
+
         return alpha;
     }
 
-    int QuiescenceSearch(Board board, int alpha, int beta)
+    private int QuiescenceSearch(Board board, int alpha, int beta, int depth)
     {
         int standPat = EvaluateBoard(board);
         if (standPat >= beta)
@@ -92,13 +86,13 @@ public class MyBot : IChessBot
         if (alpha < standPat)
             alpha = standPat;
 
-        var captureMoves = board.GetLegalMoves(false); // Assuming this returns only capturing moves
+        var moves = board.GetValidMovesForCurrentPlayer().Where(move => move.IsCapture);
 
-        foreach (var move in captureMoves)
+        foreach (var move in moves)
         {
             board.MakeMove(move);
-            int score = -QuiescenceSearch(board, -beta, -alpha);
-            board.UndoMove(move);
+            int score = -QuiescenceSearch(board, -beta, -alpha, depth + 1);
+            board.UndoLastMove();
 
             if (score >= beta)
                 return beta;
@@ -108,24 +102,68 @@ public class MyBot : IChessBot
         return alpha;
     }
 
-    int EvaluateBoard(Board board)
+    private int EvaluateBoard(Board board)
     {
-        int value = 0;
-        foreach (var pieceList in board.GetAllPieceLists())
+        string fen = board.GetFen();
+        if (_evaluatedPositions.ContainsKey(fen))
         {
-            int pieceValue;
-            switch (pieceList.TypeOfPieceInList)
+            return _evaluatedPositions[fen];
+        }
+
+        int evaluation = 0;
+
+        if (_openingBook.ContainsPosition(fen))
+        {
+            evaluation = _openingBook.GetEvaluation(fen);
+        }
+        else
+        {
+            foreach (var piece in board.WhitePieces)
             {
-                case PieceType.Pawn: pieceValue = 1; break;
-                case PieceType.Knight: pieceValue = 3; break;
-                case PieceType.Bishop: pieceValue = 3; break;
-                case PieceType.Rook: pieceValue = 5; break;
-                case PieceType.Queen: pieceValue = 9; break;
-                default: pieceValue = 0; break;
+                evaluation += GetPieceValue(piece);
             }
 
-            value += pieceList.IsWhitePieceList ? pieceValue * pieceList.Count : -pieceValue * pieceList.Count;
+            foreach (var piece in board.BlackPieces)
+            {
+                evaluation -= GetPieceValue(piece);
+            }
         }
-        return board.IsWhiteToMove ? value : -value;
+
+        _evaluatedPositions[fen] = evaluation;
+        return evaluation;
+    }
+
+    private int GetPieceValue(Piece piece)
+    {
+        // Values can be adjusted based on preference
+        return piece.Type switch
+        {
+            PieceType.Pawn => 10,
+            PieceType.Knight => 30,
+            PieceType.Bishop => 30,
+            PieceType.Rook => 50,
+            PieceType.Queen => 90,
+            PieceType.King => 900,
+            _ => 0,
+        };
+    }
+}
+
+public class OpeningBook
+{
+    // Dummy example
+    private Dictionary<string, int> _openings = new Dictionary<string, int>
+    {
+        // ["example_fen"] = evaluation
+    };
+
+    public bool ContainsPosition(string fen)
+    {
+        return _openings.ContainsKey(fen);
+    }
+
+    public int GetEvaluation(string fen)
+    {
+        return _openings.TryGetValue(fen, out int value) ? value : 0;
     }
 }
