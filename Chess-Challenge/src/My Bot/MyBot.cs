e1f6caae17165db954fbd192888f88e5b69513e5
severeduck <92829782+severeduck@users.noreply.github.com>
2023-08-26 20:33:42 +0200

    fix the "looooong thinking" bug

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,14 +1,12 @@
 ﻿using ChessChallenge.API;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public class MyBot : IChessBot
 {
     private const int QUIESCENCE_MAX_DEPTH = 2;
-    private int _maxDepth;
     private Timer _timer;
-
-    // This dictionary will only hold the UCI move strings, as the board will be provided when initializing the Move.
 
     public MyBot()
     {
@@ -17,7 +15,6 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         _timer = timer;
-        _maxDepth = CalculateDynamicDepth(timer);
 
         var legalMoves = board.GetLegalMoves().ToList();
         if (!legalMoves.Any())
@@ -25,18 +22,25 @@ public class MyBot : IChessBot
             return default; // No legal moves, which means it's either a checkmate or stalemate.
         }
 
-        int depth = 1;
+        if (board.PlyCount <= 2)
+        {
+            legalMoves = legalMoves.OrderBy(x => Guid.NewGuid()).ToList();  // Shuffle moves
+        }
+
+        int depth = board.PlyCount <= 2 ? 3 : 1;  // If it's the first or second move, start at depth 3. 
         Move bestMove = legalMoves.First();
 
+        double timeForThisMove = _timer.MillisecondsRemaining / (double)40; // Assume we'll make 40 more moves
+        DateTime startTime = DateTime.Now;
+
         // Iterative deepening
-        while (_timer.MillisecondsRemaining > 100) // Leaving a buffer of 100ms
+        while ((DateTime.Now - startTime).TotalMilliseconds < timeForThisMove && depth <= 5) // Limiting maximum depth to 5 for safety
         {
             bestMove = AlphaBetaSearch(board, legalMoves, depth++);
         }
 
         return bestMove;
     }
-
     private Move AlphaBetaSearch(Board board, List<Move> legalMoves, int depth)
     {
         Move bestMove = legalMoves.First();
@@ -77,7 +81,11 @@ public class MyBot : IChessBot
                 return beta;
 
             if (score > alpha)
+            {
                 alpha = score;
+                if (alpha >= beta)
+                    break;  // Prune the search tree.
+            }
         }
 
         return alpha;
@@ -107,32 +115,31 @@ public class MyBot : IChessBot
         return alpha;
     }
 
-private double EvaluateBoard(Board board)
-{
-    // Basic piece values
-    double pawnValue = 1.0;
-    double knightValue = 3.0;
-    double bishopValue = 3.0;
-    double rookValue = 5.0;
-    double queenValue = 9.0;
+        private double EvaluateBoard(Board board)
+    {
+        double pawnValue = 1.0;
+        double knightValue = 3.0;
+        double bishopValue = 3.0;
+        double rookValue = 5.0;
+        double queenValue = 9.0;
 
-    double whiteScore = board.GetPieceList(PieceType.Pawn, true).Count * pawnValue
-                        + board.GetPieceList(PieceType.Knight, true).Count * knightValue
-                        + board.GetPieceList(PieceType.Bishop, true).Count * bishopValue
-                        + board.GetPieceList(PieceType.Rook, true).Count * rookValue
-                        + board.GetPieceList(PieceType.Queen, true).Count * queenValue;
+        double whiteScore = board.GetPieceList(PieceType.Pawn, true).Count * pawnValue
+                            + board.GetPieceList(PieceType.Knight, true).Count * knightValue
+                            + board.GetPieceList(PieceType.Bishop, true).Count * bishopValue
+                            + board.GetPieceList(PieceType.Rook, true).Count * rookValue
+                            + board.GetPieceList(PieceType.Queen, true).Count * queenValue;
 
-    double blackScore = board.GetPieceList(PieceType.Pawn, false).Count * pawnValue
-                        + board.GetPieceList(PieceType.Knight, false).Count * knightValue
-                        + board.GetPieceList(PieceType.Bishop, false).Count * bishopValue
-                        + board.GetPieceList(PieceType.Rook, false).Count * rookValue
-                        + board.GetPieceList(PieceType.Queen, false).Count * queenValue;
+        double blackScore = board.GetPieceList(PieceType.Pawn, false).Count * pawnValue
+                            + board.GetPieceList(PieceType.Knight, false).Count * knightValue
+                            + board.GetPieceList(PieceType.Bishop, false).Count * bishopValue
+                            + board.GetPieceList(PieceType.Rook, false).Count * rookValue
+                            + board.GetPieceList(PieceType.Queen, false).Count * queenValue;
 
-    double scoreDifference = whiteScore - blackScore;
+        double scoreDifference = whiteScore - blackScore;
 
-    // Return the evaluation from the perspective of the current player
-    return board.IsWhiteToMove ? scoreDifference : -scoreDifference;
-}
+        // Return the evaluation from the perspective of the current player
+        return board.IsWhiteToMove ? scoreDifference : -scoreDifference;
+    }
 
 
     private int CalculateDynamicDepth(ChessChallenge.API.Timer timer)
