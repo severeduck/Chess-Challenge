09f275799d78550b846160e85f55ff0ad2998842
severeduck <92829782+severeduck@users.noreply.github.com>
2023-08-26 17:48:45 +0200

    disambiguate

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -139,7 +139,7 @@ public class MyBot : IChessBot
         return whiteScore - blackScore; 
     }
 
-    private int CalculateDynamicDepth(Timer timer)
+    private int CalculateDynamicDepth(ChessChallenge.API.Timer timer)
     {
         double estimatedMovesRemaining = (timer.GameStartTimeMilliseconds - timer.MillisecondsElapsedThisTurn) / (double)timer.MillisecondsElapsedThisTurn;
         double averageTimePerMove = timer.MillisecondsRemaining / estimatedMovesRemaining;
