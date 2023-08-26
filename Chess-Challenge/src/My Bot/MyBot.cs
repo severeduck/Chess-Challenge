8dd29403c969f2d623dbe41c72b6e0566f7fc524
severeduck <92829782+severeduck@users.noreply.github.com>
2023-08-26 16:53:36 +0200

    Fix the BlackIllegalMove exception, don't hardcode anything time-related

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,12 +1,11 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public class MyBot : IChessBot
 {
     private const int QUIESCENCE_MAX_DEPTH = 2;
-    private const int REGULAR_SEARCH_CUTOFF = 5000; // 5 seconds in milliseconds
-    private const int QUIESCENCE_SEARCH_CUTOFF = 3000; // 3 seconds in milliseconds
     private int _maxDepth;
     private Timer _timer;
 
@@ -17,17 +16,24 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         _timer = timer;
-        _maxDepth = CalculateDynamicDepth(board, timer);
-        return AlphaBetaSearch(board, _maxDepth);
+        _maxDepth = CalculateDynamicDepth(timer);
+        
+        var legalMoves = board.GetLegalMoves().ToList();
+        if (!legalMoves.Any())
+        {
+            return default; // No legal moves, which means it's either a checkmate or stalemate.
+        }
+
+        return AlphaBetaSearch(board, legalMoves, _maxDepth);
     }
 
-    private Move AlphaBetaSearch(Board board, int depth)
+    private Move AlphaBetaSearch(Board board, List<Move> legalMoves, int depth)
     {
         Move? bestMove = null;
         double alpha = double.NegativeInfinity;
         double beta = double.PositiveInfinity;
 
-        foreach (var move in board.GetLegalMoves())
+        foreach (var move in legalMoves)
         {
             board.MakeMove(move);
             double value = -AlphaBeta(board, depth - 1, -beta, -alpha);
@@ -40,15 +46,12 @@ public class MyBot : IChessBot
             board.UndoMove(move);
         }
 
-        if (!bestMove.HasValue)
-            throw new Exception("No legal moves available.");
-
-        return bestMove.Value;
+        return bestMove.HasValue ? bestMove.Value : legalMoves.First();
     }
 
     private double AlphaBeta(Board board, int depth, double alpha, double beta)
     {
-        if (depth == 0 || _timer.MillisecondsRemaining < REGULAR_SEARCH_CUTOFF)
+        if (depth == 0 || _timer.MillisecondsRemaining <= (_timer.GameStartTimeMilliseconds / (double)board.PlyCount))
             return QuiescenceSearch(board, alpha, beta, QUIESCENCE_MAX_DEPTH);
 
         foreach (var move in board.GetLegalMoves())
@@ -69,7 +72,7 @@ public class MyBot : IChessBot
 
     private double QuiescenceSearch(Board board, double alpha, double beta, int depth)
     {
-        if (depth == 0 || _timer.MillisecondsRemaining < QUIESCENCE_SEARCH_CUTOFF)
+        if (depth == 0 || _timer.MillisecondsRemaining <= (_timer.GameStartTimeMilliseconds / (double)board.PlyCount))
             return EvaluateBoard(board);
 
         foreach (var move in board.GetLegalMoves())
@@ -111,20 +114,17 @@ public class MyBot : IChessBot
                           + board.GetPieceList(PieceType.Rook, false).Count * rookValue
                           + board.GetPieceList(PieceType.Queen, false).Count * queenValue;
 
-        return whiteScore - blackScore; // Assuming positive is better for white.
+        return whiteScore - blackScore; 
     }
 
-    private int CalculateDynamicDepth(Board board, Timer timer)
+    private int CalculateDynamicDepth(Timer timer)
     {
-        double movesPlayed = board.PlyCount / 2.0; // Convert ply to full moves
-        double movesRemaining = 40 - movesPlayed; // Assuming a typical 40-move game for simplicity
-        if (movesRemaining <= 0) movesRemaining = 1; // To avoid division by zero
-        
-        double averageTimePerMove = timer.MillisecondsRemaining / movesRemaining;
+        double estimatedMovesRemaining = (timer.GameStartTimeMilliseconds - timer.MillisecondsElapsedThisTurn) / (double)timer.MillisecondsElapsedThisTurn;
+        double averageTimePerMove = timer.MillisecondsRemaining / estimatedMovesRemaining;
 
-        if (averageTimePerMove < 1000)
+        if (averageTimePerMove < timer.GameStartTimeMilliseconds * 0.01) // less than 1% of the total time
             return 1;
-        else if (averageTimePerMove < 5000)
+        else if (averageTimePerMove < timer.GameStartTimeMilliseconds * 0.05) // less than 5% of the total time
             return 2;
         else
             return 3; 
