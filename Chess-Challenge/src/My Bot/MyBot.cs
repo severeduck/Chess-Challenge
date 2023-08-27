8536d52cc9bc82e94d40515f24c9efcaa925cfc2
severeduck <92829782+severeduck@users.noreply.github.com>
2023-08-27 15:16:40 +0200

    protect ourselves from checkmates

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -6,7 +6,7 @@ using System.Linq;
 public class MyBot : IChessBot
 {
     const int MAX_DEPTH = 6;
-    const int MATE_DEPTH = 3;
+    const int MATE_DEPTH = 4;  // Increase mate depth
     const double PANIC_FRACTION = 0.15;
     Timer t;
 
@@ -17,7 +17,9 @@ public class MyBot : IChessBot
         var moves = b.GetLegalMoves().ToList();
         if (!moves.Any()) return default;
 
-        moves = moves.OrderByDescending(m => m.IsCapture)
+        // Prioritize moves that avoid check
+        moves = moves.OrderByDescending(m => DoesMoveAvoidCheck(m, b))
+                     .ThenByDescending(m => m.IsCapture)
                      .ThenByDescending(m => VictimValue(m.CapturePieceType) - AttackerValue(m.MovePieceType))
                      .ToList();
 
@@ -41,39 +43,82 @@ public class MyBot : IChessBot
         return best == default ? moves[new Random().Next(moves.Count)] : best;
     }
 
-    Move? MateInMoves(Board b, int n)
+    public Move? MateInMoves(Board b, int depth)
     {
         double alpha = double.NegativeInfinity;
-        foreach (var m in b.GetLegalMoves())
+        double beta = double.PositiveInfinity;
+
+        foreach (var move in b.GetLegalMoves())
         {
-            b.MakeMove(m);
-            double score = -MateSearch(b, n * 2 - 1, -double.PositiveInfinity, -alpha);
-            b.UndoMove(m);
-            if (score == double.PositiveInfinity) return m;
+            b.MakeMove(move);
+            if (b.IsInCheckmate())
+            {
+                b.UndoMove(move);
+                return move;
+            }
+
+            double score = -MateSearch(b, depth - 1, -beta, -alpha);
+
+            b.UndoMove(move);
+
+            if (score == double.PositiveInfinity) // This means we found a path that leads to a mate.
+                return move;
         }
+
         return null;
     }
 
-    double MateSearch(Board b, int d, double alpha, double beta)
+    double MateSearch(Board b, int depth, double alpha, double beta)
     {
-        if (d == 0 || b.IsInCheckmate() || b.IsDraw())
+        if (depth == 0 || b.IsDraw()) return Eval(b);
+
+        if (b.IsInCheckmate())
+            return b.IsWhiteToMove ? double.NegativeInfinity : double.PositiveInfinity;
+
+        foreach (var move in b.GetLegalMoves())
         {
-            return b.IsInCheckmate() 
-                   ? (b.IsWhiteToMove ? double.NegativeInfinity : double.PositiveInfinity)
-                   : 0;
+            b.MakeMove(move);
+            double score = -MateSearch(b, depth - 1, -beta, -alpha);
+            b.UndoMove(move);
+
+            if (score >= beta) return beta;
+            if (score > alpha) alpha = score;
         }
 
-        foreach (var m in b.GetLegalMoves())
-        {
-            b.MakeMove(m);
-            double score = -MateSearch(b, d - 1, -beta, -alpha);
-            b.UndoMove(m);
-            if (score >= beta) return beta;
-            alpha = Math.Max(alpha, score);
-        }
         return alpha;
     }
 
+    public bool DoesMoveAvoidCheck(Move move, Board board)
+    {
+        board.MakeMove(move);
+        bool isStillInCheck = board.IsInCheck();
+        board.UndoMove(move);
+        return !isStillInCheck;
+    }
+
+    double Eval(Board b)
+    {
+        double score = 0;
+
+        if (b.IsInCheckmate())
+        {
+            return b.IsWhiteToMove ? double.NegativeInfinity : double.PositiveInfinity;
+        }
+
+        score -= b.GetPieceList(PieceType.Pawn, true).GroupBy(p => p.Square.File).Where(g => g.Count() > 1).Count() * 0.5;
+        score += b.GetPieceList(PieceType.Pawn, false).GroupBy(p => p.Square.File).Where(g => g.Count() > 1).Count() * 0.5;
+
+        score += 0.1 * b.GetLegalMoves(true).Count() - 0.1 * b.GetLegalMoves(false).Count();
+
+        if (b.GetAllPieceLists().Take(6).Sum(l => l.Count()) <= 7 && b.GetAllPieceLists().Skip(6).Sum(l => l.Count()) <= 7)
+        {
+            var whiteKing = b.GetPieceList(PieceType.King, true).First().Square;
+            var blackKing = b.GetPieceList(PieceType.King, false).First().Square;
+            score += 0.3 * (7 - DistanceToCenter(whiteKing)) - 0.3 * (7 - DistanceToCenter(blackKing));
+        }
+
+        return b.IsWhiteToMove ? score : -score;
+    }
     Move Search(Board b, List<Move> moves, int d, double alpha, double beta)
     {
         Move best = moves.First();
@@ -90,7 +135,6 @@ public class MyBot : IChessBot
         }
         return best;
     }
-
     double AlphaBeta(Board b, int d, double alpha, double beta)
     {
         if (d == 0 || b.IsInCheckmate() || b.IsDraw()) return Eval(b);
@@ -112,24 +156,6 @@ public class MyBot : IChessBot
 
     int VictimValue(PieceType p) => p switch { PieceType.Pawn => 1, PieceType.Knight => 3, PieceType.Bishop => 3, PieceType.Rook => 5, PieceType.Queen => 9, _ => 0 };
     int AttackerValue(PieceType p) => p == PieceType.Pawn ? 1 : 0;
-
-    double Eval(Board b)
-    {
-        double score = 0;
-        score -= b.GetPieceList(PieceType.Pawn, true).GroupBy(p => p.Square.File).Where(g => g.Count() > 1).Count() * 0.5;
-        score += b.GetPieceList(PieceType.Pawn, false).GroupBy(p => p.Square.File).Where(g => g.Count() > 1).Count() * 0.5;
-
-        score += 0.1 * b.GetLegalMoves(true).Count() - 0.1 * b.GetLegalMoves(false).Count();
-
-        if (b.GetAllPieceLists().Take(6).Sum(l => l.Count()) <= 7 && b.GetAllPieceLists().Skip(6).Sum(l => l.Count()) <= 7)
-        {
-            var whiteKing = b.GetPieceList(PieceType.King, true).First().Square;
-            var blackKing = b.GetPieceList(PieceType.King, false).First().Square;
-            score += 0.3 * (7 - DistanceToCenter(whiteKing)) - 0.3 * (7 - DistanceToCenter(blackKing));
-        }
-
-        return b.IsWhiteToMove ? score : -score;
-    }
 
     int DistanceToCenter(Square s) => Math.Abs(4 - s.File) + Math.Abs(4 - s.Rank);
 
